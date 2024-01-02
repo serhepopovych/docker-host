c2a1a6abfc3e60cfa547833aa0a9b333d94d8b3e
Serhey Popovych <serhe.popovych@gmail.com>
2024-01-02 11:22:34 -0500

    hosts: docker: Improve process management by docker-entrypoint.sh
    Some applications, especially ones found in Debian-based images, provide
    SysV compatible initscript with corresponding infrastructure to start
    applications.
    
    We want to benefit from this infrastructure, possibly tweaking it to
    work in Docker environment correctly, rather than starting application
    manually which is common Docker approach.
    
    Furthermore some applications (e.g. nginx and lighttpd) may require
    stdout and stderr output to be proxied to docker original stdout and
    stderr file descriptors accordingly to see application output in
    "docker logs ..."; correct signal propagation to main process and it's
    subprocesses and application termination on container stop/exit.
    
    To make all above to work reliably at least following must be meet:
    
      1) job control needs to support placement of job's main process and
         all it's subprocesses to the same, dedicated process group wher
         main process designated as process group leader
    
      2) kill(1) builtin must support sending signals to process groups
         (i.e. accept -pid values that indicates signal to be sent to
         process group of whose id matches pid, not single process pid)
    
    Unfortunately the only interpreter that mets all above requirements
    is bash(1). Other interpreters such as dash(1) and mksh(1) are neither
    accept -pid to kill all processes in job process group nor kills
    reliably all processes when jobspec (i.e. with beginning with %) is
    given to kill instead of job main process id.
    
    Thus implementation based on bash(1) interpreter which is shown with
    corresponding shebang line (i.e. #!/bin/bash) at the beginning of the
    script, but also has limited compatibility with other shells such as
    dash(1) and mksh(1) where main difference compared to behavior when
    running by bash(1) is that processes in process group cannot be
    terminated by the script reliably and may remain in system/container.
    
    In this case only processes that responsible of ending it's child
    processes or have no child processes can be used reliably.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- hosts/nscache/lighttpd/docker-entrypoint.sh
+++ hosts/nscache/lighttpd/docker-entrypoint.sh
@@ -1,130 +1,328 @@
-#!/bin/sh -e
+#!/bin/sh -ue
 
-if [ -n "${PID+x}" ]; then
-    readonly timeout=10
+# Usage: sig_handler__hook <signal> ...
+sig_handler__hook()
+{
+    local signal="${1:?missing 1st arg to sig_handler__hook() <signal>}"
+    shift
 
-    # Usage: sleepx <timeout>
-    sleepx()
+    case "$signal" in
+        'EXIT')
+            # Usage: sig_handler__hook EXIT
+            exit $sig_handler__rc
+            ;;
+        'TERM')
+            # Usage: sig_handler__hook TERM <-pid> <-job>...
+            ;;
+        'KILL')
+            # Usage: sig_handler__hook KILL <-pid> <-job>...
+            ;;
+    esac
+
+    return 0
+}
+
+# Usage: sig_handler_exit [<rc>]
+sig_handler_exit()
+{
+    sig_handler__rc=${1:-$?}
+    sig_handler 'EXIT'
+}
+
+# Usage: sig_handler <signal>
+sig_handler()
+{
+    local signal="${1:?missing 1st arg to sig_handler() <signal>}"
+
+    # This is bash(1) specific, readonly, variable (array)
+    local in_bash="${BASH_VERSINFO+yes}"
+
+    # Usage: sig_handler__fatal {rc|''} <msg> ...
+    sig_handler__fatal()
     {
-        [ -n "${secs-}" ] ||
-            secs="${1:?missing 1st arg to sleepx() <timeout>}"
+        local rc=${1:-$?}   && [ -n "${1+x}" ] && shift ||
+            ${1:?missing 1st arg <rc>}
+        local msg="${1-}" && [ -n "${1+x}" ] && shift ||:
 
-        if [ $((secs -= 1)) -ge 0 ]
-        then
-            sleep 1
-        else
-            exit 121
-        fi
-    }
-
-    # wait for pid file
-    unset secs
-
-    while :; do
-        if [ -s "$PID" ]; then
-            if read -r PID _ <"$PID" &&
-               [ -n "${PID##*[!0-9]*}" ]
-            then
-                break
-            else
-                exit
-            fi
-        else
-            sleepx $timeout
-        fi
-    done
-
-    # Usage: sig_handler <signal>
-    sig_handler()
-    {
-        # not interrupting signal handler on pipeline error
-        set +e
-
-        # terminate sleep(1) job
-        kill -TERM "$!"
-
-        local signal="${1:?missing 1st arg to sig_handler() <signal>}"
-
-        case "$signal" in
-            'TERM'|'INT'|'QUIT')
-                # no reentrance
-                trap '' 'TERM' 'INT' 'QUIT'
-                unset -f sig_handler
-
-                # wait for process to exit
-                unset secs
-
-                while kill -$signal "$PID" 2>/dev/null
-                do
-                    sleepx $timeout
-                done
-
-                # now exit successfully
-                exit 0
-                ;;
-            'HUP'|'USR1'|'USR2')
-                # send signal to main $PID
-                kill -$signal "$PID"
-                ;;
-            *)
-                echo >&2 "$0: sig_handler(): unsupported signal \"$signal\""
-                exit 120
-                ;;
-        esac
-    }
-    trap 'sig_handler TERM' TERM
-    trap 'sig_handler INT' INT
-    trap 'sig_handler QUIT' QUIT
-    trap 'sig_handler HUP' HUP
-    trap 'sig_handler USR1' USR1
-    trap 'sig_handler USR2' USR2
-
-    # watch for process
-    while kill -0 "$PID"
-    do
-        sleep $timeout &
-        # interruptible by signals
-        wait
-    done
-else
-    readonly \
-        user='@user@' \
-        name='@name@' \
-        pid='/run/@name@/@named@.pid' \
-        proxy_stdio='@proxy_stdio@' \
-        #
-
-    # remove stale pid file, if any
-    rm -f "$pid" ||:
-
-    # proxy stdandard output and error using named pipe to allow
-    # daemon to write to them after switching user (e.g. using gosu)
-    if [ -n "${proxy_stdio#\@proxy_stdio\@}" ]; then
-        readonly \
-            stdout='/dev/stdout' \
-            stderr='/dev/stderr' \
+        printf >&2 -- "%s: sig_handler(): $msg\n" \
+            "$0" "$@" \
             #
 
-        # remove original symlinks
-        rm -f "$stdout" "$stderr"
+        exit $rc
+    }
 
-        # create named pipes group owned by user
-        mkfifo -m 0660 "$stdout" "$stderr"
-        chown ":$user" "$stdout" "$stderr"
+    # Usage: sig_handler__jobs <pgroup> <job>...
+    sig_handler__jobs()
+    {
+        local pgroup="${1:+-}"
+        shift
 
-        # spawn cat(1) as input/output proxy
-        cat "$stdout" &
-        cat "$stderr" >&2 &
+        jobs=''
+        while [ $# -gt 0 ]; do
+            if [ -n "${1##*[!0-9]*}" ]; then
+                jobs="${jobs:+$jobs }$pgroup$1"
+            fi
+            shift
+        done
+    }
 
-        # this serves two purposes:
-        #  1) reopens file descriptors to point to named pipes making them
-        #     available to child processes and via /proc/*/fd/*
-        #  2) opens writers to avoid sending EOF after last writer exits
-        #     (e.g. single echo >/dev/stdout will cause cat(1) exit)
-        exec >"$stdout" 2>"$stderr"
+    # Usage: sig_handler__sleep1
+    sig_handler__sleep1()
+    {
+        if [ -n "${in_bash}" ]; then
+            read -t 1 ||:
+        else
+            # This spawns new process in this script interpreter
+            # process group. Obviously this is suboptimal solution
+            # as there might be a race with kill(1) that would end
+            # sleep(1) before timeout reached.
+            sleep 1 &
+            while ! wait $!; do
+                :
+            done
+        fi
+    }
+
+    # Assert if job control isn't enabled which is default when non-interactive
+    # since background jobs aren't on separate process groups and cannot be
+    # terminated without also terminating this script interpreter process.
+    case "$-" in
+        *m*)
+            ;;
+        *)
+            sig_handler__fatal 121 'job control is OFF'
+            ;;
+    esac
+
+    case "$signal" in
+        'EXIT')
+            local pid='' jobs='' oneshot=''
+            sig_handler__hook 'EXIT' ||:
+            sig_handler__fatal 122 'should not return from sig_handler__hook()'
+            ;;
+    esac
+
+    local jobs="${jobs-}"
+    sig_handler__jobs '' $jobs
+
+    local oneshot="${oneshot-}"
+
+    local pid="${pid-}" pgrp=''
+    if [ "${pid:-0}" -gt 0 ] 2>/dev/null; then
+        pgrp="-$pid"
+    else
+        pid=''
     fi
 
-    /etc/init.d/$name start || exit
+    case "$signal" in
+        'CHLD')
+            # Our main and supplementary jobs running?
+            local p
+            for p in $pid $jobs ''; do
+                kill -CONT $p 2>/dev/null || break
+            done
+            if [ -n "$p" ]; then
+                # No, terminate remaining.
+                signal='TERM'
+            else
+                # Yes, all fine. Either continued or signal not to our jobs.
+                return 0
+            fi
+            ;;
+    esac
 
-    PID="$pid" exec gosu "$user" "$0"
+    case "$signal" in
+        'TERM'|'QUIT')
+            # No reentrance
+            trap '' 'TERM' 'QUIT' 'CHLD' 'EXIT'
+
+            # Send only $signal to the main job process: process group, if any,
+            # would be terminated in the same way as supplementary $jobs.
+            local prev_rc rc=256
+            while [ -n "$pid" ]; do
+                # Main job process
+                #   1) terminated before we kill(1) it: wait(1) has return code
+                #   2) terminated normally           : rc <= 128
+                #   3) terminated by signal          : rc = 128 + <signo> (x)
+                #   4) wait(1) interrupted by signal : rc = 128 + <signo> (x)
+                #   5) isn't our child process/job   : rc = 127
+                kill -$signal $pid 2>/dev/null || [ $rc -ge 256 ] || break
+
+                # wait(1) could be interrupted by signals received by
+                # interpreter that further may decide that signal
+                # ignored by the script (e.g. trap '' TERM);
+                # interrupted wait(1) will return 128 + <signo> status
+                while [ $rc -gt 128 ]; do
+                    prev_rc=$rc
+                    wait $pid && rc=0 || rc=$?
+                    [ $rc -ne $prev_rc ] || break
+                done
+            done
+            jobs="${oneshot:+$oneshot${jobs:+ }}$jobs"
+            jobs="${jobs:-$pgrp}"
+
+            local tries
+
+            signal='TERM'
+            tries=5
+
+            while :; do
+                # Before sending signal
+                sig_handler__hook $signal $jobs ||:
+
+                # We would send signals to process group lead first to give
+                # it time to react and end it's child processes.
+                #
+                # After that we will send signals to entire process group to
+                # terminate remaining process(es) in these groups.
+                if ! kill -$signal $jobs 2>/dev/null; then
+                    jobs=" $jobs "
+                    if [ -n "${jobs##* $pgrp *}" ]; then
+                        jobs="${jobs# }" && jobs="${jobs% }"
+                    else
+                        # Note that bash(1) does not call signal handlers set
+                        # within signal handler (i.e. trap 'trap <code> <sig>'
+                        # <sig> will not call <code> for second and further
+                        # <sig>s): exit explicitly.
+                        sig_handler_exit $rc
+                    fi
+                fi
+
+                if [ -z "${jobs##*-*}" ]; then
+                    if [  $((tries -= 1)) -gt 0 ]; then
+                        sig_handler__sleep1
+                    else
+                        signal='KILL'
+                        tries=5
+                    fi
+                else
+                    # wait(1) will sleep until process group lead(s) finished,
+                    # returning status of zero in that case, or interrupted by
+                    # script trapped signal that interpretator receives,
+                    # returning status of 128 + <signo>.
+                    while ! wait; do
+                        :
+                    done
+
+                    sig_handler__jobs '-' $pid $jobs
+                fi
+            done
+            ;;
+        'HUP'|'INT'|'USR1'|'USR2')
+            # Send signal to main job process
+            kill -$signal $pid 2>/dev/null ||:
+            ;;
+        *)
+            sig_handler__fatal 123 'unsupported signal %s' "$signal"
+            ;;
+    esac
+}
+
+################################################################################
+
+readonly \
+    user='@user@' \
+    name='@name@' \
+    pidfile='/run/@name@/@named@.pid' \
+    proxy_stdio='@proxy_stdio@' \
+    timeout=10 \
+    #
+
+# Turn on job control to put each job into separage process group
+set -m
+
+# Spawn child processes
+trap 'sig_handler TERM' TERM
+trap 'sig_handler INT' INT
+trap 'sig_handler QUIT' QUIT
+trap 'sig_handler HUP' HUP
+trap 'sig_handler USR1' USR1
+trap 'sig_handler USR2' USR2
+trap 'sig_handler CHLD' CHLD
+
+# Proxy stdandard output and error using named pipe to allow
+# daemon to write to them after switching user (e.g. using gosu)
+if [ -n "${proxy_stdio#\@proxy_stdio\@}" ]; then
+    readonly \
+        stdout='/dev/stdout' \
+        stderr='/dev/stderr' \
+        #
+
+    # Remove original symlinks
+    rm -f "$stdout" "$stderr"
+
+    # Create named pipes group owned by user
+    mkfifo -m 0660 "$stdout" "$stderr"
+    chown ":$user" "$stdout" "$stderr"
+
+    # Spawn cat(1) as input/output proxy
+    cat "$stdout" &
+    jobs="$!"
+    cat "$stderr" >&2 &
+    jobs="$jobs $!"
+
+    # This serves two purposes:
+    #  1) reopens file descriptors to point to named pipes making them
+    #     available to child processes and via /proc/*/fd/*
+    #  2) opens writers to avoid sending EOF after last writer exits
+    #     (e.g. single echo >/dev/stdout will cause cat(1) exit)
+    exec >"$stdout" 2>"$stderr"
+else
+    jobs=''
 fi
+
+# Remove stale pid file, if any
+rm -f "$pidfile" ||:
+
+# Run through SysV init script
+/etc/init.d/$name start &
+oneshot="$!"
+
+pid=''
+
+# Wait for pid file
+secs=$timeout
+while :; do
+    if [ -s "$pidfile" ]; then
+        read -r pid _ <"$pidfile" &&
+            [ "$pid" -gt 0 ] 2>/dev/null || pid=''
+        break
+    else
+        [ $((secs -= 1)) -ge 0 ] && sleep 1 || break
+    fi
+done
+
+# Watch for main process
+if [ -n "$pid" ]; then
+    space="${oneshot:+ }"
+    while kill -0 $pid; do
+        sleep $timeout &
+
+        oneshot="$oneshot$space$!"
+        while :; do
+            # interruptible by signals
+            wait $! || [ $? -le 128 ] || continue
+            break
+        done
+        oneshot="${oneshot%$space$!}"
+    done
+    unset space
+fi
+
+# Exit by sending TERM signal to self
+secs=$timeout
+while kill -TERM $$; do
+    # shouldn't be reached normally
+    sleep 1 &
+
+    while :; do
+        # interruptible by signals
+        wait $! || [ $? -le 128 ] || continue
+        break
+    done
+
+    [ $((secs -= 1)) -gt 0 ] || break
+done
+
+# Last restort
+sig_handler_exit 124
