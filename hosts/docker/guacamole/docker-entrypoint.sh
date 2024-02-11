serhepopovych/docker-host

commit 44268e36f8121dbf6c50edc033eb39029c056181

hosts: docker/guacamole: Add ldap-servers.yml for multiple LDAP servers
Having multiple DNS A RR for multiple LDAP servers support isn't enough
for guacamole: it simply picks first RR from response and sticks with it
never trying others in case of failure.

For multiple LDAP servers Guacamole uses separate ldap-servers.yml
configuration file where any ldap-* settings from guacamole.properties,
used to provide defaults, could be overwritten on per server basis using
same property name but with ldap- prefix stripped.

Since all LDAP servers share same settings in our case, ldap-servers.yml
should only contain hostames of LDAP servers, where ldap-* defaults will
be picked from guacamole.properies.

Note that for general network timeout, when for example server is not
reachable, ldap-network-timeout parameter should be used instead of
ldap-operation-timeout.

Reduce ldap-network-timeout from 30 to 10 seconds to speedup failover to
next server.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

hosts/docker/guacamole/docker-entrypoint.sh
@@ -1,0 +1,40 @@
+#!/bin/sh -e
+
+export GUACAMOLE_HOME='/tmp/.guacamole.template'
+
+# Start with a fresh GUACAMOLE_HOME. See /opt/guacamole/bin/start.sh
+# for GUACAMOLE_HOME_TEMPLATE and GUACAMOLE_HOME usage.
+
+rm -Rf "$GUACAMOLE_HOME" ||:
+install -d "$GUACAMOLE_HOME" ||:
+
+# Usage: guacamole_ldap_servers_yml
+guacamole_ldap_servers_yml()
+{
+    local ldap_hostname="${LDAP_HOSTNAME:?missing LDAP_HOSTNAME= in environment}"
+
+    getent ahosts "$ldap_hostname" | {
+        # Make network-timeout and operation-timeout given in seconds time unit
+        ldap_network_timeout=$((@ldap_network_timeout@ * 1000))
+        ldap_ips=' '
+        while read -r ip _; do
+            if [ -n "${ldap_ips##* \'$ip\' *}" ]; then
+                ldap_ips="$ldap_ips'$ip' "
+            fi
+        done
+
+        eval set -- $ldap_ips
+
+        if [ $# -gt 1 ]; then
+            printf > "$GUACAMOLE_HOME/ldap-servers.yml" -- \
+                '- hostname: %s\n' "$@" \
+                #
+            printf > "$GUACAMOLE_HOME/guacamole.properties" -- \
+                'ldap-network-timeout: %s\n' $ldap_network_timeout \
+                #
+        fi
+    }
+}
+guacamole_ldap_servers_yml
+
+exec /opt/guacamole/bin/start.sh "$@"

hosts/docker/guacamole/.dockerignore
@@ -1,0 +1,10 @@
+.git
+.gitmodules
+.gitignore
+ci
+node_modules
+npm-debug.log
+combined.log
+error.log
+.dockerignore
+Dockerfile

hosts/docker/guacamole/.gitignore
@@ -1,1 +1,2 @@
+node_modules/
 *~

hosts/docker/guacamole/Dockerfile
@@ -1,0 +1,21 @@
+ARG GUACAMOLE_TAG=latest
+FROM guacamole/guacamole:$GUACAMOLE_TAG
+
+# Parent Dockerfile has been switched to unprivileged user
+USER root
+
+## Install docker-entrypoint.sh and ecosystem.config.js.templ
+
+ARG LDAP_NETWORK_TIMEOUT=10
+
+COPY docker-entrypoint.sh /
+
+RUN sed -e "s,@ldap_network_timeout@,$LDAP_NETWORK_TIMEOUT,g" \
+        -i /docker-entrypoint.sh
+
+## Make container executable
+
+USER guacamole
+
+CMD []
+ENTRYPOINT [ "/docker-entrypoint.sh" ]

hosts/docker/guacamole/ci/up.sh
@@ -52,9 +52,13 @@ readonly \
 parse_args_usage()
 {
     cat <<EOF
+
     --ip_guacd=${ip_guacd:-<dynamic>}
         IP address in network given by --network for guacd proxy. Use
         dynamic IP if empty
+
+    --tag_guacd=$tag_guacd
+        Image tag (e.g. latest) for guacd proxy. Use default tag if empty.
 
   for guacamole:
     --gua_db_name=$gua_db_name
@@ -82,6 +86,10 @@ EOF
 # Usage: parse_args_pre ...
 parse_args_pre()
 {
+    # tag
+    tag='dh'
+    tag_guacd='latest'
+
     # read profile
     eval "local ${app}_name"
     eval "local ${app}_image"
@@ -133,12 +141,19 @@ parse_args_post()
 {
     readonly gua_ip="$ip"
     parse_args__post "$@"
+
+    tag_guacd="${tag_guacd##*:}" && tag_guacd="${tag_guacd:-latest}"
 }
 
 # Usage: parse_args_opt --<name>=[<value>]
 parse_args_opt()
 {
     case "$1" in
+        # tag
+        --tag_guacd=*)
+            arg "$1"
+            ;;
+
         # network
         --ip_guacd=*)
             arg "$1" 'non-empty-value'
@@ -218,7 +233,7 @@ up()
     local name_guacamole="guac-guacamole${inst:+_$inst}"
     local image_guacamole="${_app}/guacamole:$tag"
     local name_guacd="guac-guacd${inst:+_$inst}"
-    local image_guacd="${_app}/guacd:$tag"
+    local image_guacd="${_app}/guacd:$tag_guacd"
     local name
     local image
 
@@ -226,13 +241,10 @@ up()
 
     name="$name_guacamole" \
         docker_rm
-    name="$name_guacd" \
-        docker_rm
 
-    image="$image_guacamole" \
-        docker_pull
-    image="$image_guacd" \
-        docker_pull
+    name="$name_guacamole" image="$image_guacamole" \
+        docker_build "$build_args" \
+            #
 
     # Usage: mysql_init_db
     mysql_init_db()
@@ -275,6 +287,12 @@ up()
             #
 
     ## guacd
+
+    name="$name_guacd" \
+        docker_rm
+
+    image="$image_guacd" \
+        docker_pull
 
     image="$image_guacd" \
         docker_volume \

commit 649138ef607ccbf5fa8eb45a0817bb50ceeff497

hosts: docker/guacamole: Skip multiple LDAP servers setup when no LDAP
If no LDAP configuration needed (e.g. docker-run(1) early for database
initialization using /opt/guacamole/bin/initdb.sh) do not treat missing
environment variable LDAP_HOSTNAME= as fatal error in function intended
to configure multiple LDAP servers through ldap-servers.yml.

Fixes: 44268e36f812 ("hosts: docker/guacamole: Add ldap-servers.yml for multiple LDAP servers")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

hosts/docker/guacamole/docker-entrypoint.sh
@@ -11,7 +11,8 @@ install -d "$GUACAMOLE_HOME" ||:
 # Usage: guacamole_ldap_servers_yml
 guacamole_ldap_servers_yml()
 {
-    local ldap_hostname="${LDAP_HOSTNAME:?missing LDAP_HOSTNAME= in environment}"
+    local ldap_hostname="${LDAP_HOSTNAME-}"
+    [ -n "$ldap_hostname" ] || return 0
 
     getent ahosts "$ldap_hostname" | {
         # Make network-timeout and operation-timeout given in seconds time unit