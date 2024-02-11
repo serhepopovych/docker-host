serhepopovych/docker-host

commit 641a2c9d954ce7db303774714341c89ade72cc17

hosts: docker/sftp: Introduce and use assert_m() helper
To catch when job control isn't available, but required.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

hosts/docker/sftp/docker-entrypoint.sh
@@ -3,6 +3,21 @@
 # Requires: nc.openbsd(1), gawk(1), chroot(1), sleep(1), mkfifo(1), cat(1)
 #           chown(1), rm(1)
 
+# Usage: assert_m
+assert_m()
+{
+    # Assert if job control isn't enabled which is default when non-interactive
+    # since background jobs aren't on separate process groups and cannot be
+    # terminated without also terminating this script interpreter process.
+    case "$-" in
+        *m*)
+            ;;
+        *)
+            : "${m:?job control is OFF}"
+            ;;
+    esac
+}
+
 # Usage: mksleepfd
 mksleepfd()
 {
@@ -84,6 +99,9 @@ mksleepfd()
     if rmfifo "$fl" && [ -n "${sleepjob-&}" ]; then
         # Not polluting callers namespace
         unset -f rmfifo
+
+        # Job control required
+        assert_m
 
         # File descriptors, especially standard input, inherited
         # by child job running sleepfd(), but it will open named
@@ -188,6 +206,9 @@ sleepx()
                     # fallback to sleep(1) due to errors or data being read(1)
                     in_bash=''
                 else
+                    # Job control required
+                    assert_m
+
                     # This spawns new process in this script interpreter
                     # process group. Obviously this is suboptimal solution
                     # as there might be a race with kill(1) that would end
@@ -396,16 +417,8 @@ sig_handler()
         jobs="${jobs# }" && jobs="${jobs:+ $jobs}"
     }
 
-    # Assert if job control isn't enabled which is default when non-interactive
-    # since background jobs aren't on separate process groups and cannot be
-    # terminated without also terminating this script interpreter process.
-    case "$-" in
-        *m*)
-            ;;
-        *)
-            sig_handler__fatal 121 'job control is OFF'
-            ;;
-    esac
+    # Job control required
+    assert_m
 
     case "$signal" in
         'EXIT')

commit 89c0901450d11176f6ed41cc133d35a5d9ec312e

hosts: docker/sftp: Improve assert_m() helper
It might be difficult, or nearly impossibe to determine function name
while tracing scriptwith "set -x" to determine what function triggered
assert_m().

Accept optionally first parameter that may for example specify function
name that called it to make it possible to find the caller.

Add way to optout assertion by settings $assert_m__ignore variable when
job control turned off intentionally.

Fixes: 641a2c9d954c ("hosts: docker/sftp: Introduce and use assert_m() helper")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

hosts/docker/sftp/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 # Requires: nc.openbsd(1), gawk(1), chroot(1), sleep(1), mkfifo(1), cat(1)
 #           chown(1), rm(1)
 
-# Usage: assert_m
+# Usage: assert_m [<func>]
 assert_m()
 {
     # Assert if job control isn't enabled which is default when non-interactive
@@ -13,7 +13,10 @@ assert_m()
         *m*)
             ;;
         *)
-            : "${m:?job control is OFF}"
+            local m="${assert_m__ignore+yes}"
+            local func="${1-}"
+
+            : "${m:?${func:+$func: }job control is OFF}"
             ;;
     esac
 }
@@ -101,7 +104,7 @@ mksleepfd()
         unset -f rmfifo
 
         # Job control required
-        assert_m
+        assert_m 'mksleepfd'
 
         # File descriptors, especially standard input, inherited
         # by child job running sleepfd(), but it will open named
@@ -207,7 +210,7 @@ sleepx()
                     in_bash=''
                 else
                     # Job control required
-                    assert_m
+                    assert_m 'sleepx'
 
                     # This spawns new process in this script interpreter
                     # process group. Obviously this is suboptimal solution
@@ -286,6 +289,7 @@ syslog_cat()
         # no need to place each job into separate process group here:
         # subshell is a part of pipeline in parent process group
         set +m
+        assert_m__ignore=1
 
         printf -- '<45>syslog_cat: syslog proxy v0.1' "$0"
 
@@ -380,7 +384,9 @@ sig_handler_exit()
 # Usage: sig_handler <signal>
 sig_handler()
 {
-    local signal="${1:?missing 1st arg to sig_handler() <signal>}"
+    local func='sig_handler'
+
+    local signal="${1:?missing 1st arg to $func() <signal>}"
 
     # Usage: sig_handler__fatal {rc|''} <msg> ...
     sig_handler__fatal()
@@ -389,7 +395,7 @@ sig_handler()
             ${1:?missing 1st arg <rc>}
         local msg="${1-}" && [ -n "${1+x}" ] && shift ||:
 
-        printf >&2 -- "%s: sig_handler(): $msg\n" \
+        printf >&2 -- "%s: $func(): $msg\n" \
             "$0" "$@" \
             #
 
@@ -418,7 +424,7 @@ sig_handler()
     }
 
     # Job control required
-    assert_m
+    assert_m "$func"
 
     case "$signal" in
         'EXIT')